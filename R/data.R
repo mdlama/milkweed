5f11dde02741dfcbd89eb7e5aa07960fc55dc103
M. Drew LaMar <mdlama@wm.edu>
2019-05-13 21:11:59 -0400

    Cleaned up unused data
    Also added MIT license and updated description file

--- R/data.R
+++ R/data.R
@@ -36,16 +36,6 @@
 
 #' Seed data!
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
-#'
-#' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
-#' @source \url{http://www.diamondse.info/}
 "seeddata"
 
 #'
